799e5ab27236453f41b46c0342a4dc6c111d8d92
baldcat <babaiev.ihor@gmail.com>
2022-03-08 22:40:45 +0200

    Loged request

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Services\Telegram\Commands\Start;
 use App\Services\Telegram\Request;
+use Illuminate\Support\Facades\Log;
 
 class WebhookController extends Controller
 {
@@ -11,7 +12,9 @@ class WebhookController extends Controller
     public function index(Request $request)
     {
 
+        Log::info('111');
         if ($request->input('message') == Start::ROUTE) {
+            Log::info('222');
             app('start')->start();
         } else {
             app('main-summary')->list();
