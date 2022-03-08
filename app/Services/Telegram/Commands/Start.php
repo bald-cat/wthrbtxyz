749cfedfce4f06abbc645c6444abfc3f6b00f62a
baldcat <babaiev.ihor@gmail.com>
2022-03-08 22:39:33 +0200

    Loged request

--- app/Services/Telegram/Commands/Start.php
+++ app/Services/Telegram/Commands/Start.php
@@ -5,6 +5,7 @@ namespace App\Services\Telegram\Commands;
 use App\Services\Telegram\Message;
 use App\Services\Telegram\Request;
 use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\Log;
 
 class Start
 {
@@ -20,10 +21,12 @@ class Start
         $this->message = $message;
         $this->answer = Lang::get('commands.start');
         $this->chat_id = $request->input('chat_id');
+        Log::info($request);
     }
 
-    public function start($chat_id)
+    public function start()
     {
+        Log::info('1');
         $this->message->setChatId($this->chat_id)->setText($this->answer)->send();
     }
 
