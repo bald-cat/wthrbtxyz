e10ef9b0445fd591902f7447cec9857199409aa9
baldcat <babaiev.ihor@gmail.com>
2022-03-10 14:03:57 +0200

    added Orchid laravel admin panel

---
+++ app/Orchid/Layouts/TelegramUsers.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Orchid\Layouts;
+
+use Orchid\Screen\Layouts\Table;
+use Orchid\Screen\TD;
+
+class TelegramUsers extends Table
+{
+    /**
+     * Data source.
+     *
+     * The name of the key to fetch it from the query.
+     * The results of which will be elements of the table.
+     *
+     * @var string
+     */
+    protected $target = 'telegram_users';
+
+    /**
+     * Get the table cells to be displayed.
+     *
+     * @return TD[]
+     */
+    protected function columns(): array
+    {
+        return [
+
+            TD::make('name', 'Имя пользователя')
+                ->align('left'),
+            TD::make('chat_id', 'id чата')->align('left'),
+            TD::make('language_code', 'Язык кода'),
+            TD::make('created_at', 'Добавлен'),
+        ];
+    }
+}
---
+++ app/Orchid/Screens/TelegramUser.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Orchid\Screens;
+
+use App\Orchid\Layouts\TelegramUsers;
+use Orchid\Screen\Layouts\Table;
+use Orchid\Screen\Screen;
+use Orchid\Screen\TD;
+use Orchid\Support\Facades\Layout;
+
+class TelegramUser extends Screen
+{
+    /**
+     * Query data.
+     *
+     * @return array
+     */
+    public function query(): iterable
+    {
+        return [
+           'users_count' => \App\Models\TelegramUser::query()->count(),
+            'telegram_users' => \App\Models\TelegramUser::all(),
+        ];
+    }
+
+    /**
+     * Display header name.
+     *
+     * @return string|null
+     */
+    public function name(): ?string
+    {
+        return 'Список пользователей';
+    }
+
+    /**
+     * Button commands.
+     *
+     * @return \Orchid\Screen\Action[]
+     */
+    public function commandBar(): iterable
+    {
+        return [];
+    }
+
+    /**
+     * Views.
+     *
+     * @return \Orchid\Screen\Layout[]|string[]
+     */
+    public function layout(): iterable
+    {
+        return [
+           Layout::metrics([
+               'Общее кол-во пользователей' => 'users_count'
+           ]),
+
+            Layout::blank([TelegramUsers::class]),
+
+        ];
+    }
+}
