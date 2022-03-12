7c0471b1b00e9e81b1b6c901f4d17b0da6377a10
baldcat <babaiev.ihor@gmail.com>
2022-03-12 20:47:25 +0200

    Edited start command

--- app/Services/Telegram/Commands/Start.php
+++ app/Services/Telegram/Commands/Start.php
@@ -27,6 +27,8 @@ class Start
     public function start()
     {
         $this->message->setChatId($this->chat_id)->setText($this->answer)->send();
+        $this->message->setChatId($this->chat_id)->setText("<b>Вот что я умею в данный момент:</b>")->send();
+        $this->message->setChatId($this->chat_id)->setText("Определение погоды в городе по его названию \xF0\x9F\x8C\x87")->send();
 
         $replyMarkup = [
             'keyboard' => [
@@ -41,7 +43,7 @@ class Start
 
         TelegramRequest::request('sendMessage', [
             'chat_id' => $this->chat_id,
-            'text' => "<b>Определение местоположения</b> \xF0\x9F\x8C\x8D",
+            'text' => "Определение погоды на основе местоположения \xF0\x9F\x8C\x8D",
             'reply_markup' => json_encode($replyMarkup),
             'parse_mode' => 'HTML',
         ]);
