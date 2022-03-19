fd4cf92df7df55c5f99d43456b63c14967a244ab
baldcat <babaiev.ihor@gmail.cpm>
2022-03-19 16:41:40 +0200

    Try to fix request

--- app/Services/Telegram/Request.php
+++ app/Services/Telegram/Request.php
@@ -32,8 +32,8 @@ class Request
         $this->map = [
             'chat_id' => $this->request['message']['chat']['id'] ?? null,
             'message' => $this->request['message']['text'] ?? null,
-            'username' => $this->request['message']['from']['username'] ?? null,
-            'language_code' => $this->request['message']['from']['language_code'] ?? null,
+            'username' => $this->request['message']['from']['username'] ?? 'NOT DATA',
+            'language_code' => $this->request['message']['from']['language_code'] ?? 'NOT DATA',
             'longitude' => $this->request['message']['location']['longitude'] ?? null,
             'latitude' => $this->request['message']['location']['latitude'] ?? null,
         ];
