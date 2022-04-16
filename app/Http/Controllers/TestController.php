af8b987ab3969e54106309c1f8e45ea95343b97c
baldcat <babaiev.ihor@gmail.com>
2022-04-16 20:08:20 +0300

    Send info about update

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\TelegramRequest;
 use App\Models\TelegramUser;
+use App\Services\Telegram\Compilations\MainSummary;
 use App\Services\Telegram\Message;
 
 class TestController extends Controller
@@ -11,12 +13,18 @@ class TestController extends Controller
     public function test()
     {
 
-        $text = "<br>Обновление!<br> Теперь информация о погоде выводится одним сообщением.";
+        $text = "<b>Обновление!<b> Теперь информация о погоде выводится одним сообщением. Для примера погода в Киеве:";
         $message = new Message();
         $users = TelegramUser::all();
 
         foreach ($users as $user) {
+            TelegramRequest::request('sendMessage', [
+                'chat_id' => $user->chat_id,
+                'text' => "*Обновление!* Теперь информация о погоде выводится одним сообщением. Для примера погода в Киеве:",
+                'parse_mode' => 'Markdown',
+            ]);
             $message->setText($text)->setChatId($user->chat_id)->send();
+            (new MainSummary($user->chat_id, 'Киев'))->list();
         }
 
 
