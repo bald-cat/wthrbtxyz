404d3673e0a3f915fcfed5720c0c4a790f10ccf8
bald-cat <babaiev.ihor@gmail.com>
2022-04-20 19:33:43 +0300

    Added config with emojis

---
+++ config/emojis.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    'digit' => [
+        'one' => "\x31\xE2\x83\xA3",
+        'two' => "\x32\xE2\x83\xA3",
+        'three' => "\x33\xE2\x83\xA3",
+        'four' => "\x34\xE2\x83\xA3",
+        'five' => "\x35\xE2\x83\xA3",
+        'six' => "\x36\xE2\x83\xA3",
+        'seven' => "\x37\xE2\x83\xA3",
+        'eight' => "\x38\xE2\x83\xA3",
+        'nine' => "\x39\xE2\x83\xA3",
+        'zero' => "\x30\xE2\x83\xA3",
+    ],
+
+    'winter-deg' => [
+        'N' => "\xE2\xAC\x87",
+        'S' => "\xE2\xAC\x86",
+        'W' => "\xE2\x9E\xA1",
+        'E' => "\xE2\xAC\x85",
+
+    ],
+
+    'wind-speed' => "\xF0\x9F\x8D\x83",
+
+    'sunrise' => "\xF0\x9F\x8C\x85",
+    'sunset' => "\xF0\x9F\x8C\x87",
+    'celsius' => "&#8451;",
+    'feels-like' => "\xE2\x81\x89",
+    'pin' => "\xF0\x9F\x93\x8C",
+
+    'time' => [
+        'morning' => "\xF0\x9F\x94\xB8",
+        'day' => "\xF0\x9F\x94\xB9",
+        'evening' => "\xF0\x9F\x94\xB8",
+        'night' => "\xF0\x9F\x94\xB9",
+    ]
+
+];
